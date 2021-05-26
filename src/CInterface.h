5c4101899a26ac63ca2703e1dbbf0e5492866df7
machavi4 <machavi4@fit.cvut.cz>
2021-05-26 20:29:54 +0200

    implementace interfacu

--- src/CInterface.h
+++ src/CInterface.h
@@ -3,16 +3,35 @@
 //
 
 #pragma once
-
 #include <iostream>
 #include <string>
+#include <utility>
 
-class CInterface{
+class CInterface {
  public:
+  CInterface(std::ostream& out, std::istream& in) : m_Out(out), m_In(in) {}
+  template <class returnType>
+  returnType PromtWithMessage(const std::string& message) const;
 
+  void Message(const std::string& message) const;
 
+  template <class T>
+  std::ostream& Print(const T toPrint);
 
  private:
-  std::ostream & m_Out;
-  std::istream & m_In;
+  std::ostream& m_Out;
+  std::istream& m_In;
 };
+template <class returnType>
+returnType CInterface::PromtWithMessage(const std::string& message) const {
+  returnType res;
+  m_Out << message << std::endl;
+  res << m_In;
+  return res;
+}
+template <class T>
+std::ostream& CInterface::Print(const T toPrint) {
+  m_Out << toPrint;
+  return m_Out;
+}
+void CInterface::Message(const std::string& message) const { m_Out << message; }
